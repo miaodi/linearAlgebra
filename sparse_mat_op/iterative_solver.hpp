de527988938921e6392a85d57a9cd6c7633ce2d6
Di Miao <miaodi1987@gmail.com>
2025-08-08 22:33:31 -0700

    added stop criteria for gmres

--- sparse_mat_op/iterative_solver.hpp
+++ sparse_mat_op/iterative_solver.hpp
@@ -12,10 +12,10 @@ namespace iterative_solver {
 template <typename VALTYPE>
 void givens_rotation(VALTYPE *const R, VALTYPE *const g, VALTYPE *const c,
                      VALTYPE *const s, const VALTYPE beta, VALTYPE &resid,
-                     const int lda, const int j) {
+                     const size_t lda, const size_t j) {
   auto R_col_j = R + j * lda;
   // apply Givens rotation to R_col_j
-  for (int i = 0; i < j; i++) {
+  for (size_t i = 0; i < j; i++) {
     auto tmp = c[i] * R_col_j[i] - s[i] * R_col_j[i + 1];
     R_col_j[i + 1] = s[i] * R_col_j[i] + c[i] * R_col_j[i + 1];
     R_col_j[i] = tmp;
@@ -36,24 +36,24 @@ void givens_rotation(VALTYPE *const R, VALTYPE *const g, VALTYPE *const c,
   resid *= s[j];
 }
 
+enum class State : int {
+  CONVERGED = 0,
+  RUNNING = 1,
+  MAX_ITER_REACHED = 2,
+  FAILED = 3
+};
 template <typename VALTYPE> class GMRES {
 public:
-  enum class ErrorType : int {
-    NO_ERROR = 0,
-    MAX_ITER_REACHED = 1,
-    INSUFFICIENT_MEMORY = 2,
-    INVALID_MATRIX = 3
-  };
   GMRES() {}
 
-  void setMaxIter(int max_iter) { _max_iter = max_iter; }
+  void setMaxIter(size_t max_iter) { _max_iter = max_iter; }
   void setAbsTol(VALTYPE abs_tol) { _abs_tol = abs_tol; }
   void setRelTol(VALTYPE rel_tol) { _rel_tol = rel_tol; }
-  void setRestart(int restart) { _restart = restart; }
+  void setRestart(size_t restart) { _restart = restart; }
 
   template <matrix_utils::SpmvOpType Op, matrix_utils::PrecOpType PrecOp>
-  ErrorType operator()(Op const *const op, PrecOp const *const prec,
-                       VALTYPE const *b, VALTYPE *x) {
+  State operator()(Op const *const op, PrecOp const *const prec,
+                   VALTYPE const *b, VALTYPE *x) {
 
     static_assert(std::is_same_v<typename Op::VALTYPE, VALTYPE>,
                   "Op::VALTYPE must be the same as VALTYPE");
@@ -61,6 +61,7 @@ public:
                   "PrecOp::VALTYPE must be the same as VALTYPE");
 
     const auto size = op->size();
+    _restart = std::min(static_cast<size_t>(size), _restart);
     const auto restart1 = _restart + 1;
     _H.resize(_restart, _restart);
     _H.setZero();
@@ -74,27 +75,27 @@ public:
     Eigen::Map<Eigen::Matrix<VALTYPE, Eigen::Dynamic, 1>> x_vec(x, size);
 
     VALTYPE resid, init_resid, beta;
-    ErrorType error_code = ErrorType::NO_ERROR;
+    State state_code = State::RUNNING;
 
     vec_ops::copy_vec(size, b, _tmp.data());
+
     // b-= Ax_0
     (*op)(x, _tmp.data(), (VALTYPE)(-1), (VALTYPE)(1));
-    std::cout << _tmp << std::endl;
-
     // resid_0 = M^{-1}(b - Ax_0)
     (*prec)(_tmp.data(), _Q.col(0).data());
-    std::cout << _Q << std::endl;
-    int j, iter;
+
+    init_resid = resid = _Q.col(0).norm();
+    if (init_resid < _abs_tol) {
+      return State::CONVERGED;
+    }
+
+    size_t j, iter;
     for (iter = 0; iter < _max_iter;) {
-      init_resid = resid = _Q.col(0).norm();
-      if (init_resid < _abs_tol) {
-        return error_code;
-      }
-      _Q.col(0) = _Q.col(0) / init_resid;
+      _Q.col(0) = _Q.col(0) / resid;
 
       for (j = 0; j < _restart; j++, iter++) {
         if (iter >= _max_iter) {
-          error_code = ErrorType::MAX_ITER_REACHED;
+          state_code = State::MAX_ITER_REACHED;
           break;
         }
         // v_j_ptr = A * v_j_ptr
@@ -102,7 +103,7 @@ public:
         // Apply preconditioner
         (*prec)(_tmp.data(), _Q.col(j + 1).data());
 
-        for (int i = 0; i <= j; i++) {
+        for (size_t i = 0; i <= j; i++) {
           // H[i][j] = v_i^T * v_j
           _H(i, j) = _Q.col(i).dot(_Q.col(j + 1));
           // w -= H[i][j] * v_i
@@ -116,10 +117,11 @@ public:
                         _restart, j);
         std::cout << "iter: " << iter << " "
                   << "resid: " << std::abs(resid) << " "
-                  << "relative resid: " << std::abs(resid) / init_resid << std::endl;
+                  << "relative resid: " << std::abs(resid) / init_resid
+                  << std::endl;
         if (std::abs(resid) < _abs_tol ||
             std::abs(resid) < _rel_tol * init_resid) {
-          error_code = ErrorType::NO_ERROR;
+          state_code = State::CONVERGED;
           break;
         }
       }
@@ -127,22 +129,25 @@ public:
           .template triangularView<Eigen::Upper>()
           .solveInPlace(_g.head(j));
       x_vec += _Q.leftCols(j) * _g.head(j);
-
+      if (state_code != State::RUNNING) {
+        break;
+      }
       vec_ops::copy_vec(size, b, _tmp.data());
       // b-= Ax_i
       (*op)(x, _tmp.data(), (VALTYPE)(-1), (VALTYPE)(1));
       // resid_i = b - Ax_i
       (*prec)(_tmp.data(), _Q.col(0).data());
+      resid = _Q.col(0).norm();
     }
 
-    return error_code;
+    return state_code;
   }
 
 private:
-  int _max_iter{1000};
+  size_t _max_iter{100};
   VALTYPE _abs_tol{0.0};
   VALTYPE _rel_tol{1e-8};
-  int _restart{100};
+  size_t _restart{20};
   Eigen::Matrix<VALTYPE, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> _H;
   Eigen::Matrix<VALTYPE, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> _Q;
   Eigen::Matrix<VALTYPE, Eigen::Dynamic, 1> _g;
